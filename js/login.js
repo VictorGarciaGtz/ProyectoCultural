4d0439e004036d1a3fcf3b8ce6a0f0fbb4665e85
VictorGarciaGtz <victor.garciagutierrez.itcm@gmail.com>
2019-07-23 14:53:19 -0500

    Eliminacion de residuos login

--- js/login.js
+++ js/login.js
@@ -1,3 +1,26 @@
+$(document).ready(function(){
+    $('#btnOlvidarContra').on('click', recuperarContra);
+});
+
+$(function(){
+    $(".validar").keydown(function(event){
+        //alert(event.keyCode);
+        if((event.keyCode < 48 || event.keyCode > 57) && (event.keyCode < 96 || event.keyCode > 105) && event.keyCode !==190  && event.keyCode !==110 && event.keyCode !==8 && event.keyCode !==9  ){
+            return false;
+        }
+    });
+});
+
+function validar_email( email ) 
+{
+    var regex = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    return regex.test(email) ? true : false;
+}
+
+function recuperarContra(){
+
+}
+
 // Sign-in success callback
 function onSuccess(googleUser) {
     // Get the Google profile data (basic)
@@ -62,38 +85,3 @@ function userCheck(userData){
 }
 
 
-$(document).ready(function(){
-   /*  $('#cardRegistrarse').hide(); */
-    /* $('#btnMostrarRegistrarse').on("click", mostrarRegistrarse); */
-    /* $('#btnMostrarInicioS').on("click", mostrarInicioSesion); */
-    $('#btnOlvidarContra').on('click', recuperarContra);
-});
-
-/* function mostrarRegistrarse(){
-    $('#cardIniciarSesion').hide();
-    $('#cardRegistrarse').show('fade');
-}
-
-function mostrarInicioSesion(){
-    $('#cardRegistrarse').hide();
-    $('#cardIniciarSesion').show('fade');
-} */
-
-$(function(){
-    $(".validar").keydown(function(event){
-        //alert(event.keyCode);
-        if((event.keyCode < 48 || event.keyCode > 57) && (event.keyCode < 96 || event.keyCode > 105) && event.keyCode !==190  && event.keyCode !==110 && event.keyCode !==8 && event.keyCode !==9  ){
-            return false;
-        }
-    });
-});
-
-function validar_email( email ) 
-{
-    var regex = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-    return regex.test(email) ? true : false;
-}
-
-function recuperarContra(){
-
-}
--- login.php
+++ login.php
@@ -49,91 +49,12 @@
                                 </div>
                             </form>
                         </div>
-                        <div class="form-group row my-2 justify-content-center mx-2" id="gSignIn">
-                            <!-- <div style="" class="abcRioButton abcRioButtonBlue col-12 col-md-5">
-                                <div class="abcRioButtonContentWrapper">
-                                    <div class="abcRioButtonIcon" style="padding:8px">
-                                        <div style="width:18px;height:18px;" class="abcRioButtonSvgImageWithFallback abcRioButtonIconImage abcRioButtonIconImage18">
-                                            <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="18px" height="18px" viewBox="0 0 48 48" class="abcRioButtonSvg">
-                                                <g><path fill="#EA4335" d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z"></path>
-                                                    <path fill="#4285F4" d="M46.98 24.55c0-1.57-.15-3.09-.38-4.55H24v9.02h12.94c-.58 2.96-2.26 5.48-4.78 7.18l7.73 6c4.51-4.18 7.09-10.36 7.09-17.65z"></path>
-                                                    <path fill="#FBBC05" d="M10.53 28.59c-.48-1.45-.76-2.99-.76-4.59s.27-3.14.76-4.59l-7.98-6.19C.92 16.46 0 20.12 0 24c0 3.88.92 7.54 2.56 10.78l7.97-6.19z"></path>
-                                                    <path fill="#34A853" d="M24 48c6.48 0 11.93-2.13 15.89-5.81l-7.73-6c-2.15 1.45-4.92 2.3-8.16 2.3-6.26 0-11.57-4.22-13.47-9.91l-7.98 6.19C6.51 42.62 14.62 48 24 48z"></path>
-                                                    <path fill="none" d="M0 0h48v48H0z"></path>
-                                                </g>
-                                            </svg>
-                                        </div>
-                                    </div>
-                                    <span style="font-size:13px;line-height:34px;" class="abcRioButtonContents">
-                                        <span id="not_signed_incfms09rp89cw">Sign in with Google</span>
-                                        <span id="connectedcfms09rp89cw" style="display:none">Signed in with Google</span>
-                                    </span>
-                                </div>
-                            </div> -->
-                        
-                        </div>                       
-                        <!-- <div class="form-group row my-2 justify-content-center mx-2">
-                            <button type="button" class="btn btn-secondary col-12 col-md-5" id="btnMostrarRegistrarse">Registrarse</button>
-                        </div> -->
+                        <div class="form-group row my-2 justify-content-center mx-2" id="gSignIn"></div>                                             
                         <div class="my-1">
                             <button type="button" class="btn btn-link" id="btnOlvidarContra">Olvide mi contraseña</button>
                         </div>
                     </div>
                 </div><!-- aqui termina el inicio de sesion -->
-                <!-- <div class="card text-center shadow-bottom-right" id="cardRegistrarse">
-                    <img src="img/Instituto-Cultural-Tampico-.png" class="logoCultural">
-                    <div class="card-body">
-                        <p class="my-2 sesionFont text-center">Registrarse</p>
-                        <div class="form-cont">
-                            <form action="">
-                                <div class="form-group row justify-content-center mx-2">
-                                    <input type="text" class="form-control col-12 col-md-10 validar" id="matricula" name="matricula"  required/>
-                                    <label for="matricula"><i class="fa fa-user-o iconos" aria-hidden="true"></i>Matricula</label>
-                                </div>
-                                <div class="form-group row justify-content-center mx-2">
-                                    <input type="text" class="form-control col-12 col-md-10" id="nombreCompleto" name="nombreCompleto"  required/>
-                                    <label for="nombreCompleto">Nombre Completo</label>
-                                </div>
-                                <div class="form-group row justify-content-center mx-2">
-                                    <input type="email" class="form-control col-12 col-md-10" id="correo" name="correo"  required/>
-                                    <label for="correo">Correo</label>
-                                    
-                                </div>
-                                <div class="form-group row justify-content-center mx-2">
-                                    <input type="text" class="form-control col-12 col-md-5 mr-2 validar" id="telefono" name="telefono"  required/>
-                                    <label for="telefono">Telefono</label>
-                                    <select class="form-control col-12 col-md-5" id="rol">
-                                        <option>-- Seleccione un rol --</option>
-                                        <option>Profesor</option>
-                                        <option>Tutor</option>
-                                    </select>
-                                </div>
-                                <div class="form-group row justify-content-center mx-2">
-                                    <select class="form-control col-12 col-md-10" id="campus">
-                                        <option>-- Seleccione el campus al que pertenece --</option>
-                                        <option>Campus I</option>
-                                        <option>Campus II</option>
-                                        <option>Campus III</option>
-                                    </select>
-                                </div>
-                                <div class="form-group row justify-content-center mx-2">
-                                    <input type="password" class="form-control col-12 col-md-10" id="contraseña" name="contraseña" required>
-                                    <label for="contraseña">Contraseña</label>
-                                </div>
-                                <div class="form-group row justify-content-center mx-2">
-                                    <input type="password" class="form-control col-12 col-md-10" id="nvacontraseña" name="nvacontraseña" required>
-                                    <label for="nvacontraseña">Confirmar Contraseña</label>
-                                </div>
-                                <div class="form-group row my-2 justify-content-center mx-2">
-                                    <input type="submit" value="Registrarse" class="btn btn-iniciarSesion col-12 col-md-5">
-                                </div>
-                            </form>
-                        </div>
-                        <div class="form-group row my-2 justify-content-center mx-2">
-                                <button type="button" class="btn btn-secondary col-12 col-md-5" id="btnMostrarInicioS">Iniciar Sesión</button>
-                        </div>
-                    </div> -->
-                </div> <!-- Aqui termina la card de registrarse -->
             </div>
         </div>
     </div>
@@ -141,8 +62,7 @@
     <script src="js/jquery-3.4.1.min.js"></script>
     <script src="js/popper.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
-    <script>
-    
+    <script> 
     // Render Google Sign-in button
         function renderButton() {
         gapi.signin2.render('gSignIn', {
